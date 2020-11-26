b4cd13c5828eb8f32a25e6f2882564ac5be4563c
Aurelian Shuttleworth <aurelian.shuttleworth@sprinthive.com>
2020-11-26 19:51:53 +0200

    Inital Commit

---
+++ homeassistant/components/investec_beta/const.py
@@ -0,0 +1,7 @@
+"""Constants for the investec_beta integration."""
+
+DOMAIN = "investec_beta"
+
+# TODO Update with your own urls
+OAUTH2_AUTHORIZE = "https://www.example.com/auth/authorize"
+OAUTH2_TOKEN = "https://www.example.com/auth/token"
